948ec53aa753d06dfcc14457c2f63dd1841f2c1e
Jesus <jesus.joanaazuara@usp.ceu.es>
2021-04-10 10:09:22 +0200

    Subo el proyecto tipoAbstractoDeDatos al repositorio remoto.

---
+++ src/main/java/ceu/usp/pr2/App.java
@@ -0,0 +1,13 @@
+package ceu.usp.pr2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
